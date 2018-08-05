7a805cd2e372857d6f4a56557924d9407b74b256
eric11235 <jecooney@gmail.com>
2018-08-04 23:44:38 -0400

    Upload social media homework #7

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "euTG5abV9WcSA1fOE8qGuZbZp"
+consumer_secret = "ORbQTOULcHeS8zHH4d0kB0dNesVnjsr4ehBrF91zCCTbkZPukD"
+access_token = "1016722062621241344-jpz7Ip2UR8BmPD2gDX5It5Im2NuzyE"
+access_token_secret = "M6AujB5xhkB9HI9TQPjKoPaNaUt5jyXnNAWC2zFBAYujX"
